938969a97e77e828987d48c28188dfa24d3d7727
Neil Seejoor <nvseejoor@gmail.com>
2017-09-21 00:19:52 +0000

    old code

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,4 @@
 def using_until
         levitation_force = 6
         #your code here
-    
 end
-
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,6 @@
-def using_while
-	levitation_force = 6
-	
+i = 6
+def using_while i < 10
+	puts "Wingardium Leviosa"
+  i += 1
 	#your code here
 end
-
-
